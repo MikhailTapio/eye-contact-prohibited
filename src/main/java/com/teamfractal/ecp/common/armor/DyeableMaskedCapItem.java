7caf0a8fd5479f2aa9bf26d3adad96ccc4d9a626
MikhailTapio <mikhailtapio13468@163.com>
2021-12-06 22:58:44 +0800

    Initialization and release

---
+++ src/main/java/com/teamfractal/ecp/common/armor/DyeableMaskedCapItem.java
@@ -0,0 +1,37 @@
+package com.teamfractal.ecp.common.armor;
+
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.monster.EnderMan;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.*;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.common.extensions.IForgeItem;
+
+import javax.annotation.Nonnull;
+import java.text.MessageFormat;
+import java.util.List;
+
+public class DyeableMaskedCapItem extends ArmorItem implements DyeableLeatherItem, IForgeItem {
+    public DyeableMaskedCapItem(ArmorMaterial a, EquipmentSlot s, Properties p) {
+        super(a, s, p);
+    }
+
+    @Override
+    public boolean isEnderMask(ItemStack stack, Player player, EnderMan endermanEntity)
+    {
+        return true;
+    }
+
+    @Override
+    public void appendHoverText(@Nonnull ItemStack itemstack, Level world, @Nonnull List<Component> list, @Nonnull TooltipFlag flag) {
+        list.add(new TextComponent(MessageFormat.format
+                (new TranslatableComponent("tooltip.ecp.masked_armor").getString()
+                        , new TranslatableComponent("types.ecp.cap").getString())).withStyle(ChatFormatting.AQUA));
+        super.appendHoverText(itemstack, world, list, flag);
+    }
+}
---
+++ src/main/java/com/teamfractal/ecp/common/armor/ECPArmorMaterials.java
@@ -0,0 +1,93 @@
+package com.teamfractal.ecp.common.armor;
+
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.util.LazyLoadedValue;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
+
+import javax.annotation.Nonnull;
+import java.util.function.Supplier;
+
+public enum ECPArmorMaterials  implements ArmorMaterial {
+    LEATHER_MASKED("leather_masked", 5, new int[]{1, 2, 3, 1}, 15, SoundEvents.ARMOR_EQUIP_LEATHER, 0.0F, 0.0F, () -> {
+        return Ingredient.of(Items.LEATHER);
+    }),
+    CHAIN_MASKED("chainmail_masked", 15, new int[]{1, 4, 5, 2}, 12, SoundEvents.ARMOR_EQUIP_CHAIN, 0.0F, 0.0F, () -> {
+        return Ingredient.of(Items.IRON_INGOT);
+    }),
+    IRON_MASKED("iron_masked", 15, new int[]{2, 5, 6, 2}, 9, SoundEvents.ARMOR_EQUIP_IRON, 0.0F, 0.0F, () -> {
+        return Ingredient.of(Items.IRON_INGOT);
+    }),
+    GOLD_MASKED("gold_masked", 7, new int[]{1, 3, 5, 2}, 25, SoundEvents.ARMOR_EQUIP_GOLD, 0.0F, 0.0F, () -> {
+        return Ingredient.of(Items.GOLD_INGOT);
+    }),
+    DIAMOND_MASKED("diamond_masked", 33, new int[]{3, 6, 8, 3}, 10, SoundEvents.ARMOR_EQUIP_DIAMOND, 2.0F, 0.0F, () -> {
+        return Ingredient.of(Items.DIAMOND);
+    }),
+    TURTLE_MASKED("turtle_masked", 25, new int[]{2, 5, 6, 2}, 9, SoundEvents.ARMOR_EQUIP_TURTLE, 0.0F, 0.0F, () -> {
+        return Ingredient.of(Items.SCUTE);
+    }),
+    NETHERITE_MASKED("netherite_masked", 37, new int[]{3, 6, 8, 3}, 15, SoundEvents.ARMOR_EQUIP_NETHERITE, 3.0F, 0.1F, () -> {
+        return Ingredient.of(Items.NETHERITE_INGOT);
+    });
+
+    private static final int[] HEALTH_PER_SLOT = new int[]{13, 15, 16, 11};
+    private final String name;
+    private final int durabilityMultiplier;
+    private final int[] slotProtections;
+    private final int enchantmentValue;
+    private final SoundEvent sound;
+    private final float toughness;
+    private final float knockbackResistance;
+    private final LazyLoadedValue<Ingredient> repairIngredient;
+
+    ECPArmorMaterials(String p_40474_, int p_40475_, int[] p_40476_, int p_40477_, SoundEvent p_40478_, float p_40479_, float p_40480_, Supplier<Ingredient> p_40481_) {
+        this.name = p_40474_;
+        this.durabilityMultiplier = p_40475_;
+        this.slotProtections = p_40476_;
+        this.enchantmentValue = p_40477_;
+        this.sound = p_40478_;
+        this.toughness = p_40479_;
+        this.knockbackResistance = p_40480_;
+        this.repairIngredient = new LazyLoadedValue<>(p_40481_);
+    }
+
+    public int getDurabilityForSlot(EquipmentSlot p_40484_) {
+        return HEALTH_PER_SLOT[p_40484_.getIndex()] * this.durabilityMultiplier;
+    }
+
+    public int getDefenseForSlot(EquipmentSlot p_40487_) {
+        return this.slotProtections[p_40487_.getIndex()];
+    }
+
+    public int getEnchantmentValue() {
+        return this.enchantmentValue;
+    }
+
+    @Nonnull
+    public SoundEvent getEquipSound() {
+        return this.sound;
+    }
+
+    @Nonnull
+    public Ingredient getRepairIngredient() {
+        return this.repairIngredient.get();
+    }
+
+    @Nonnull
+    public String getName() {
+        return this.name;
+    }
+
+    public float getToughness() {
+        return this.toughness;
+    }
+
+    public float getKnockbackResistance() {
+        return this.knockbackResistance;
+    }
+
+}
